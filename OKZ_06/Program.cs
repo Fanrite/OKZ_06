3beecbe3c6405a4b454634e4eeb9897dea6d9f83
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-04-15 20:12:51 +0300

    +3 zavd

--- OKZ_06/Program.cs
+++ OKZ_06/Program.cs
@@ -64,8 +64,8 @@ namespace OKZ_06
 
             int ch, a = 0;
             string[] mas1 = new string[32] { "а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ь", "ы", "ъ", "э", "ю", "я" };
-            int[] mas2 = new int[32]       {  1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,  16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 };
-             
+            int[] mas2 = new int[32]       {  1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,  32 };
+              
             string str1 = new string(' ', 0);
             string str2 = new string(' ', 0);
             str1 = text;
@@ -74,8 +74,7 @@ namespace OKZ_06
             int[] kod = new int[ch];
             int[] vyv = new int[ch];
             Random rnd = new Random();
-            
-                // заполняем массив двумерный
+ 
                 for (int i = 0; i < ch; i++)
                 {
                     for (int j = 0; j < ch; j++)
@@ -83,7 +82,7 @@ namespace OKZ_06
                         mas3[i, j] = rnd.Next(0, 9);
                     }
                 }
-                // записываем текст в числа
+ 
                 for (int q = 0; q < ch; q++)
                 {
                     for (int z = 0; z < 32; z++)
@@ -95,7 +94,7 @@ namespace OKZ_06
                         }
                     }
                 }
-                // кодируем 
+ 
                 for (int z = 0; z < ch; z++)
                 {
                     for (int x = 0; x < ch; x++)
@@ -105,7 +104,7 @@ namespace OKZ_06
                     vyv[z] = a;
                 }
             string temp1 = "", temp2, temp3 = "";
-                // выводим на экран закодировонное сообщение
+ 
                 foreach (int d in vyv)
                 {
                     temp1 += (d.ToString() + "  ");
@@ -128,9 +127,58 @@ namespace OKZ_06
     }
 
     
-
     class Program
     {
+        public static void Heal(string text, ref  string output)
+        {
+            char[] mas1 = new char[32] { 'а',  'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я' };
+            int[] mas2 = new int[32] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 };
+            int[,] key = new int[3, 3]
+            {
+                { 1, 5, 2 },
+                { 3, 5, 9 },
+                { 6, 8, 4 }
+            };
+
+            int[] temp = new int[text.Length];
+            char[] tc = text.ToCharArray();
+            int l = text.Length / 3;
+
+            for (int i = 0; i < tc.Length; i++)
+                for (int j = 0; j < 32; j++)
+                    if (tc[i] == mas1[j]) temp[i] = mas2[j];
+
+            int[,] temp2 = new int[l, 3];
+
+            int buf = 0;
+            for (int i = 0; i < l; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    temp2[i, j] = temp[buf];
+                    buf++;
+                }
+                       
+            int[,] temp3 = new int[l, 3];
+            
+            for (int i = 0; i < l; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    temp3[i, j] = (temp2[i, 0] * key[j, 0] + temp2[i, 1] * key[j, 1] + temp2[i, 2] * key[j, 2]) % 32;
+                }
+
+
+
+            for (int i = 0; i < l; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    for (int g = 0; g < 32; g++)
+                        if (temp3[i, j] == mas2[g]) output += mas1[g];
+
+                output += " ";
+            }
+
+        }
+
         static void Main(string[] args)
         {
             int vb = Convert.ToInt32(Console.ReadLine());
@@ -156,8 +204,15 @@ namespace OKZ_06
 
             else if (vb == 3)
             {
+                string text, output = "";
+                Console.Write("Введіть повідомлення:");
+                text = Console.ReadLine();
 
+                Heal(text, ref output);
 
+                Console.WriteLine($"Зашифроване  повідомлення:{output}");
+                Console.WriteLine($"Розшифроване повідомлення:{text}");
+                Console.ReadKey();
             }
         }
     }
